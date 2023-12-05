a1b1c55cbd9cfd739b7f3e68f587b11b251beb70
Demis Previotto <demis.previotto@gmail.com>
2023-12-05 16:46:52 +0100

    index order

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.use(cors());
 app.use("/posts", require("./routes/posts"));
 app.use("/users", require("./routes/users"));
 app.use("/comments", require("./routes/comments"));
+app.use("/orders", require("./routes/orders"));
 app.use("uploads", express.static("uploads"));
 
 app.use(handleTypeError);
